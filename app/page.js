46f7c1144e24ba09b4b526e921d6fdc93768a093
Malik Piara <malik@hey.com>
2025-11-08 00:19:15 +0000

    Removed dark mode and made black the default background.

--- app/page.js
+++ app/page.js
@@ -3,19 +3,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center font-sans bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-black sm:items-start">
 
 
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black text-zinc-50">
            We're RoundTwenty.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          <p className="max-w-md text-lg leading-8 text-zinc-400">
             We build products that foster connection, enable education and create more access to opportunities.
           </p>
           <div>
-            <h1 className="max-w-xs text-2xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            <h1 className="max-w-xs text-2xl font-semibold leading-10 tracking-tight text-zinc-50">
            Our Communities
           </h1>
           <div className="flex gap-3 flex-col leading-12">
