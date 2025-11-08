62db795fd3edd38871d23f3635791317816339c9
Malik Piara <malik@hey.com>
2025-11-08 00:40:24 +0000

    Added shadcn + created cards for projects.

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
-import Image from "next/image";
 import Link from "next/link";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
   return (
@@ -8,29 +16,48 @@ export default function Home() {
 
 
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black text-zinc-50">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-zinc-50">
            We're RoundTwenty.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-400">
             We build products that foster connection, enable education and create more access to opportunities.
           </p>
-          <div>
-            <h1 className="max-w-xs text-2xl font-semibold leading-10 tracking-tight text-zinc-50">
+          <div className="w-full">
+            <h1 className="max-w-xs text-2xl font-semibold leading-10 tracking-tight text-zinc-50 mb-5">
            Our Communities
           </h1>
-          <div className="flex gap-3 flex-col leading-12">
-            <div className="flex flex-col">
-              <Link href='https://lisboaux.com/'>LisboaUX</Link>
-              </div>
-              <div className="flex flex-col">
-              <Link href='http://lisboajs.org/'>LisboaJS</Link>
-              </div>
-              <div className="flex flex-col">
-              <Link href='https://outono.org/'>Outono</Link>
+          <div className="flex gap-3 flex-col leading-12 w-full">
+
+              <Link href='https://lisboaux.com/'>
+              <Card className=''>
+  <CardHeader>
+    <CardTitle>LisboaUX</CardTitle>
+    <CardDescription>Design Community</CardDescription>
+  </CardHeader>
+</Card>
+  </Link>
+
+   <Link href='https://lisboajs.org/'>
+              <Card className=''>
+  <CardHeader>
+    <CardTitle>LisboaJS</CardTitle>
+    <CardDescription>JavaScript Community</CardDescription>
+  </CardHeader>
+</Card>
+  </Link>
+   <Link href='https://outono.org/'>
+              <Card className=''>
+  <CardHeader>
+    <CardTitle>Outono</CardTitle>
+    <CardDescription>Portuguese Diaspora</CardDescription>
+  </CardHeader>
+</Card>
+  </Link>
+
               </div>
           </div>
           </div>
-        </div>
+
 
       </main>
     </div>
